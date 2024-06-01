126462076ad425acc4906043d0af4a75deb96322
Naphthalina <bnassefuah@st.ug.edu.gh>
2024-06-01 16:38:12 -0700

    Included placeholder for Search

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,36 +1,47 @@
-
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
 import { Octicons } from '@expo/vector-icons';
-
 
 export default function SearchBar() {
   return (
     <View style={styles.container}>
-        <TextInput style={styles.searchBar}
-        placeholder="Search"
-        placeholderTextColor='black'>
-        <Octicons name="search" size={24} color="black" />
-        </TextInput>
+      <View style={styles.searchBarContainer}>
+        <Octicons name="search" size={24} color="black" style={styles.searchIcon} />
+        <TextInput
+          style={styles.searchBar}
+          placeholder="Search"
+          placeholderTextColor="gray"
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop:30,
-    marginLeft:20,
-    marginRight:20,  
+    marginTop: 30,
+    marginLeft: 20,
+    marginRight: 20,
   },
-  searchBar: {
-    height: 49,
+  searchBarContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
     borderColor: '#ccc',
     borderWidth: 1,
     borderRadius: 30,
-    paddingLeft: 15,
-    border:1 ,
-    marginBottom: 16,
     backgroundColor: 'white',
-    color:'black'
-   // width:353,
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  searchBar: {
+    flex: 1,
+    height: 49,
+    color: 'black',
+    paddingLeft: 40, // Add padding to make space for the icon
+  },
+  searchIcon: {
+    position: 'absolute',
+    left: 15,
+    zIndex: 1,
   },
 });
